7ab36d1945e5f58a410f979afe42f09c3ee09e28
mohdhusainahmed001 <mohdhusainahmed001@gmail.com>
2026-04-21 12:22:38 +0530

    updated app.py & test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,5 +4,5 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello, CI/CD Pipeline with Python!" in response.data
+    assert b"Hello sir!, Welcome to onextel!" in response.data
 
